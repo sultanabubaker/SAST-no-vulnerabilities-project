c9277726a5205d65e21c2efa5fb214a412c24a9f
Sultan Abu Baker <87023622+sultanabubaker@users.noreply.github.com>
2022-04-06 16:07:37 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,12 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+puplic class main {
+
+	public static void main(String[] args) {
+		Logger log = LogManager.getLogger();
+		log.info(s: "Request User Agent:{}", o:"test");
+
+	}
+}
